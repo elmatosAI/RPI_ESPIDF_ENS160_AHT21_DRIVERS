f9851bc97208f52515bf85f649faa7edddceb145
elmatosAI <emilio.lopez.matos@gmail.com>
2025-03-03 06:33:15 +0000

    Addedtext_publish to debug web page

---
+++ main/test_publish.c
@@ -0,0 +1,8 @@
+// filepath: /workspaces/RPI_ESPIDF_ENS160_AHT21_DRIVERS/main/test_publish.c
+#include "publish.h"
+
+int main() {
+    // Start the web server
+    start_web_server();
+    return 0;
+}
